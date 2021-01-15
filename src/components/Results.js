2e8d217d93b53ce4b73a5480e1e876e084a65730
Goran Tenic <goran@pop-os.localdomain>
2021-01-15 20:25:36 +0100

    api request and showing result on screen

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import UrlForm from "./UrlForm";
+import LoadingSpinner from "./LoadingSpinner";
 
 function Results() {
+  const [shortLinks, setShortlinks] = useState([]);
+  const [loading, setLoading] = useState(false);
   return (
     <section className="results container">
-      <UrlForm />
-      <div hidden>
-        <div className="result">
-          <p className="original-url">https://frontendmentor.io</p>
-          <p className="short-url">https://rel.ink/4ksa</p>
-          <button className="btn t-green">Copy</button>
-        </div>
-        <div className="result">
-          <p className="original-url">https://frontendmentor.io</p>
-          <p className="short-url">https://rel.ink/4ksa</p>
-          <button className="btn t-green">Copy</button>
-        </div>
-        <div className="result">
-          <p className="original-url">https://frontendmentor.io</p>
-          <p className="short-url">https://rel.ink/4ksa</p>
-          <button className="btn t-green">Copy</button>
-        </div>
+      <UrlForm
+        setLoading={setLoading}
+        shortLinks={shortLinks}
+        setShortlinks={setShortlinks}
+      />
+      <div>
+        {loading && <LoadingSpinner />}
+        {shortLinks.length > 0 &&
+          shortLinks.map((link) => (
+            <div key={link.shortLink} className="result">
+              <p className="original-url">{link.originalLink}</p>
+              <p className="short-url">{link.shortLink}</p>
+              <button className="btn t-green">Copy</button>
+            </div>
+          ))}
       </div>
     </section>
   );
--- src/components/UrlForm.js
+++ src/components/UrlForm.js
@@ -1,25 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-function UrlForm() {
+/*
+error_code
+1 	No URL specified ("url" parameter is empty)
+2 	Invalid URL submitted
+3 	Rate limit reached. Wait a second and try again
+4 	IP-Address has been blocked because of violating our terms of service
+5 	shrtcode code (slug) already taken/in use
+6 	Unknown error
+7 	No code specified ("code" parameter is empty)
+8 	Invalid code submitted (code not found/there is no such short-link)
+9 	Missing required parameters
+10 	Trying to shorten a disallowed Link. More information on disallowed links
+*/
+
+const API_URL = "https://api.shrtco.de/v2/";
+
+function shortenPath(url) {
+  return `/shorten?url=${url}`;
+}
+
+function UrlForm({ shortLinks, setShortlinks, setLoading }) {
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+
+  async function handleSubmit(e) {
+    const URL_REGEX = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
+    setError("");
+    setLoading(false);
+    e.preventDefault();
+    if (!URL_REGEX.test(url) || url.length === 0) {
+      return setError("Please enter a valid url.");
+    }
+
+    setLoading(true);
+    try {
+      const response = await fetch(`${API_URL}${shortenPath(url)}`);
+      let data;
+      if (response.ok) {
+        data = await response.json();
+      } else {
+        console.log("error", data);
+      }
+
+      const link = {
+        originalLink: data.result.original_link,
+        shortLink: data.result.full_short_link,
+      };
+
+      setShortlinks([...shortLinks, link]);
+      setUrl("");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error.message);
+    }
+  }
+
   return (
-    <form className="url-form">
+    <form onSubmit={handleSubmit} className="url-form">
       <label className="sr-only" htmlFor="url">
         URL
       </label>
-      <input
-        type="text"
-        name="url"
-        id="url"
-        placeholder="Shorten a link here..."
-      />
-      <p className="error-message" hidden>
-        Please add a link
-      </p>
+      <div className="form-group">
+        <input
+          value={url}
+          onChange={(e) => {
+            setError("");
+            setUrl(e.target.value);
+          }}
+          type="text"
+          name="url"
+          id="url"
+          placeholder="Shorten a link here..."
+        />
+        {error.length > 0 && <p className="error-message">{error}</p>}
+      </div>
       <button type="submit" className="btn bg-green">
         Shorten It!
       </button>
     </form>
   );
 }
+
+UrlForm.propTypes = {
+  shortLinks: PropTypes.array.isRequired,
+  setShortlinks: PropTypes.func.isRequired,
+  setLoading: PropTypes.func.isRequired,
+};
 
 export default UrlForm;
