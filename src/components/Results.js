1b9b7949bea17de9816e0ac5588dd648351038bf
Goran Tenic <goran@pop-os.localdomain>
2021-01-16 20:59:40 +0100

    localstorage

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,10 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import UrlForm from "./UrlForm";
 import LoadingSpinner from "./LoadingSpinner";
+
+const STORAGE_KEY = "shortLinks";
 
 function Results() {
   const [shortLinks, setShortlinks] = useState([]);
   const [loading, setLoading] = useState(false);
+
+  const memoizedShortlinks = useMemo(() => {
+    return shortLinks;
+  }, [shortLinks]);
+
+  useEffect(() => {
+    const links = JSON.parse(localStorage.getItem(STORAGE_KEY)) || [];
+    setShortlinks(links);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(memoizedShortlinks));
+  }, [memoizedShortlinks]);
+
+  function handleCopy(link) {
+    navigator.clipboard.writeText(link);
+  }
+
   return (
     <section className="results container">
       <UrlForm
@@ -19,7 +39,12 @@ function Results() {
             <div key={link.shortLink} className="result">
               <p className="original-url">{link.originalLink}</p>
               <p className="short-url">{link.shortLink}</p>
-              <button className="btn t-green">Copy</button>
+              <button
+                onClick={() => handleCopy(link.shortLink)}
+                className="btn t-green"
+              >
+                Copy
+              </button>
             </div>
           ))}
       </div>
